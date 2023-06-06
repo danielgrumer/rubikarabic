76bde77ea4dccc33deb1ea19d6ca6b2424f91e91
Yanone <post@yanone.de>
2023-06-06 14:56:25 +0200

    Deleted original Rubik files

--- old/version-2/sources/report.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-
-gftools qa -f ../fonts/vf/Rubik[wght].ttf  -gfb --fontbakery --diffenator -o ../fonts/vf/out_roman
-
-gftools qa -f ../fonts/vf/Rubik-Italic[wght].ttf  -gfb --fontbakery --diffenator -o ../fonts/vf/out_italic
